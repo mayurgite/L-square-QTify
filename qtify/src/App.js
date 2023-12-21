11d39db4928911825486be6e88ee87882da9b0f6
mayurgite.it@gmail.com <mayurgite.it@gmail.com>
2023-12-21 12:33:20 +0530

    Test commit

---
+++ qtify/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./components/Button/Button";
+import Search from "./components/Search/Search";
+
+function App() {
+  return (
+    <>
+    <Search placeholder={"Search a album of your choice"}/>
+    </>
+  );
+}
+
+export default App;

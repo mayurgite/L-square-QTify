d7d68d5efe17d158c5aff31d10a78caaf2b7e253
mayurgite.it@gmail.com <mayurgite.it@gmail.com>
2023-12-22 22:07:42 +0530

    test

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-//import Hero from "./components/Hero/Hero";
+import Hero from "./components/Hero/Hero";
 function App() {
   return (
     <>
     <Navbar SearchData={""}/>
-    
+    <Hero/>
   </>
   );
 }
